1c4f64c7b6681d1fbfe2af49507efbb1b71f9b97
Info Qoin <info@qoin.co.jp>
2026-07-05 22:31:57 +0900

    Remove generated artifacts from source control

--- .netlify/static/_next/static/chunks/app/resident/layout-0096e201ac6526dd.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[96],{8175:function(n,e,t){Promise.resolve().then(t.bind(t,4976))},4976:function(n,e,t){"use strict";t.r(e),t.d(e,{default:function(){return r}});var u=t(7437);function r(n){let{children:e}=n;return(0,u.jsx)(u.Fragment,{children:e})}t(2265)}},function(n){n.O(0,[971,23,744],function(){return n(n.s=8175)}),_N_E=n.O()}]);
